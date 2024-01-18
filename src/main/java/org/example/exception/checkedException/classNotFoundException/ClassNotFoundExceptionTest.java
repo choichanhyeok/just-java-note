d191d05c0183219647b838c23f80358af1eee75d
hyeoking <cksgurwkd12@naver.com>
2024-01-18 14:38:44 +0900

    feat: ClassNotFoundException test 중 잘못 작성된 예제 주석처리

--- src/main/java/org/example/exception/checkedException/classNotFoundException/ClassNotFoundExceptionTest.java
+++ src/main/java/org/example/exception/checkedException/classNotFoundException/ClassNotFoundExceptionTest.java
@@ -30,17 +30,18 @@ public class ClassNotFoundExceptionTest {
         });
     }
 
-    @DisplayName("직렬화 실패")
-    @Test
-    protected void test3(){
-        Assertions.assertThrows(ClassNotFoundException.class, () -> {
-            byte[] serializedData = {/* 직렬화 데이터 */};
-
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serializedData);
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-            objectInputStream.readObject(); // ClassNotFoundException 발생
-        });
-    }
+//    @DisplayName("직렬화 실패")
+//    @Test
+//    protected void test3(){
+//        Assertions.assertThrows(ClassNotFoundException.class, () -> {
+//            // 존재하지 않는 클래스의 직렬화된 데이터를 가정
+//            byte[] serializedData = {/* 직렬화 데이터 */};
+//
+//            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serializedData);
+//            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+//            objectInputStream.readObject(); // ClassNotFoundException 발생
+//        });
+//    }
 
     class dummy{}
 }
