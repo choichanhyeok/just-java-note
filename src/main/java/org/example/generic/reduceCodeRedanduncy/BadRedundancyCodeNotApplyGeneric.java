156e54b9d7f02bfdc9a8a6e2da68614e4bc9ea19
hyeoking <cksgurwkd12@naver.com>
2024-01-18 19:09:15 +0900

    studying: How to make low redundancy code on Generic

---
+++ src/main/java/org/example/generic/reduceCodeRedanduncy/BadRedundancyCodeNotApplyGeneric.java
@@ -0,0 +1,38 @@
+﻿package org.example.generic.reduceCodeRedanduncy;
+
+public class BadRedundancyCodeNotApplyGeneric {
+
+    private void processEachBox() {
+        StringBox stringBox = new StringBox();
+        stringBox.setContent("Hi");
+
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.setContent(100);
+
+        System.out.println("content: " + stringBox.getContent() + " " + integerBox.getContent());
+    }
+
+    class StringBox {
+        private String content;
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+
+    class IntegerBox {
+        private Integer content;
+
+        public void setContent(Integer content) {
+            this.content = content;
+        }
+
+        public Integer getContent() {
+            return content;
+        }
+    }
+}
---
+++ src/main/java/org/example/generic/reduceCodeRedanduncy/CoolRedundancyCodeApplyGeneric.java
@@ -0,0 +1,26 @@
+﻿package org.example.generic.reduceCodeRedanduncy;
+
+public class CoolRedundancyCodeApplyGeneric {
+
+    private void processEachBox(){
+        Box<String> stringBox = new Box<>();
+        Box<Integer> integerBox = new Box<>();
+
+        stringBox.setContent("Hyeok");
+        integerBox.setContent(29);
+
+        System.out.println("content: " + stringBox.getContent() + " " + integerBox.getContent());
+    }
+
+    class Box<T> {
+        private T content;
+
+        private T getContent(){
+            return content;
+        }
+
+        private void setContent(T content) {
+            this.content = content;
+        }
+    }
+}
