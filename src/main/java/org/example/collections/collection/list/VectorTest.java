3ed926a4be42dfe48ef1eb770885a83ad918eb72
hyeok <cksgurwkd12@naver.com>
2024-01-23 08:17:44 +0900

    feat: Fail Log, is Vector save synchronized test

--- src/main/java/org/example/collections/collection/list/VectorTest.java
+++ src/main/java/org/example/collections/collection/list/VectorTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Vector;
+import java.util.concurrent.*;
 
 public class VectorTest {
-    Vector<String> vectorForThreadTest = new Vector<>();
+    Vector<Integer> vectorForThreadTest = new Vector<>();
+    final int ITERATOR_SIZE = 100;
+    final int[] answer = new int[ITERATOR_SIZE];
 
     public VectorTest(){
         this.test1();
@@ -33,48 +36,47 @@ public class VectorTest {
         });
     }
 
-//    @DisplayName("Vector 학습용 테스트(2): Integer Type, Vector has thread safety.")
-//    @Test
-//    protected void test2() throws ExecutionException, InterruptedException {
-//
-//        ExecutorService executorService = Executors.newFixedThreadPool(2);
-//        Future<?> future1 = executorService.submit(new ThreadDemoFirst());
-//        Future<?> future2 = executorService.submit(new ThreadDemoSecond());
-//
-//        future1.get();
-//        future2.get();
-//
-//        Thread.sleep(10000);
-//        Assertions.assertEquals("hi", vectorForThreadTest.get(0));
-//        Assertions.assertEquals("great", vectorForThreadTest.get(1));
-//        Assertions.assertEquals("hyeok", vectorForThreadTest.get(2));
-//    }
+    @DisplayName("Vector 학습용 테스트(2): Integer Type, Vector has thread safety.")
+    @Test
+    protected void test2() throws InterruptedException, ExecutionException, TimeoutException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future future1 = executorService.submit(new ThreadDemoFirst());
+        Future future2 = executorService.submit(new ThreadDemoSecond());
+
+        future1.get(1, TimeUnit.SECONDS);
+        future2.get(1, TimeUnit.SECONDS);
+
+        // 현실적으로 불가능 판단.
+    }
 
 
 
     class ThreadDemoFirst extends Thread{
-        public void run(){
-            vectorForThreadTest.add("hi");
-
+        public void run() {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            vectorForThreadTest.add("great");
+            for (int i = 0; i < ITERATOR_SIZE; i ++){
+                vectorForThreadTest.add(i);
+            }
         }
     }
 
     class ThreadDemoSecond extends Thread{
         public void run() {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(15);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            vectorForThreadTest.add("hyeok");
+            for (int i = 0 ; i < ITERATOR_SIZE; i ++){
+                answer[i] = vectorForThreadTest.get(i);
+            }
         }
     }
 }
