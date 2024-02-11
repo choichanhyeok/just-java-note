1221c743094705d9fe26aa153f83061b11da83e3
hyeok <cksgurwkd12@naver.com>
2024-02-11 13:13:44 +0900

    feat: written servlet format

---
+++ src/main/java/org/example/servletAndJSP/servletJSP.java
@@ -0,0 +1,20 @@
+package org.example.servletAndJSP;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class servletJSP {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>안녕하세요!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
