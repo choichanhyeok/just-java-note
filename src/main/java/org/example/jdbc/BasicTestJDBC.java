5c9e8e74209c2d640d6681aae87228ac0043c15c
hyeok <cksgurwkd12@naver.com>
2024-02-12 18:15:32 +0900

    feat: written jdbc basic test

--- src/main/java/org/example/jdbc/BasicTestJDBC.java
+++ src/main/java/org/example/jdbc/BasicTestJDBC.java
@@ -7,12 +7,12 @@ import java.sql.*;
 
 public class BasicTestJDBC {
 
-    @DisplayName("BasicTestJDBC: 데이터베이스 연동 테스트")
+    @DisplayName("BasicTestJDBC(1): 데이터베이스 연동 테스트")
     @Test
     protected void test1(){
         String url = "jdbc:mysql://localhost:3306/test";
         String user = "root";
-        String password = "1541";
+        String password = "Slqlrjf12#$";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
@@ -41,6 +41,70 @@ public class BasicTestJDBC {
         } catch (SQLException e){
             e.printStackTrace();
             System.out.println("fail connection DataBase !!");
+        }
+    }
+
+    @DisplayName("BasicTestJDBC(2): 데이터베이스")
+    @Test
+    public void test2(){
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "Slqlrjf12#$";
+
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("success connection DATABASE !!");
+
+            // TODO 1. 작업 진행
+            String query = "SELECT * FROM student WHERE name = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "hyeok");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+               String data = resultSet.getString("name");
+               System.out.println(data);
+            }
+
+            // TODO 2. 자원 해제
+            resultSet.close();
+            preparedStatement.close();
+
+            // TODO 3. 연결 종료
+            connection.close();
+            System.out.println("disconnection DATABASE !!");
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("fail connection DATABASE !!");
+        }
+
+    }
+
+
+    @DisplayName("BasicTestJDBC(3): ")
+    @Test
+    protected void test3(){
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "Slqlrjf12#$";
+
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            String sql = "SELECT * FROM STUDENT WHERE name = \'hyeok\'";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                System.out.println(rs.getString("name"));
+            }
+
+            rs.close();
+            stmt.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("fail Database transaction");
         }
     }
 }
