d05e07819b448d7f04fe340ed99ea2d934de897a
hyeok <cksgurwkd12@naver.com>
2024-02-13 12:29:34 +0900

    feat: 인덱스 생성 예시

--- src/main/java/org/example/jdbc/BasicTestJDBC.java
+++ src/main/java/org/example/jdbc/BasicTestJDBC.java
@@ -139,4 +139,46 @@ public class BasicTestJDBC {
 //
 //
 //    }
+
+
+    @DisplayName("BasicTestJDBC(5): 인덱스 생성 예시")
+    @Test
+    protected void test5() {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String password = "Slqlrjf12#$";
+
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            String dropTableQuery = "DROP TABLE IF EXISTS users;";
+
+            String createTableQuery = "CREATE TABLE users (\n" +
+                    "    user_id INT PRIMARY KEY,\n" +
+                    "    last_name VARCHAR(50),\n" +
+                    "    first_name VARCHAR(50)\n" +
+                    ");";
+
+            String createIndexQuery = "CREATE INDEX idx_last_name ON users(last_name);\n";
+            String insertQuery = "INSERT INTO users (user_id, last_name, first_name) VALUES (100, 'Smith', 'fuck');";
+            String findUserData = "SELECT * FROM users WHERE last_name = 'Smith';\n";
+
+            Statement stmt = connection.createStatement();
+            stmt.execute(dropTableQuery);
+            stmt.execute(createTableQuery);
+            stmt.execute(createIndexQuery);
+            stmt.execute(insertQuery);
+            ResultSet rs = stmt.executeQuery(findUserData);
+
+            while(rs.next()){
+                System.out.println(rs.getString("last_name"));
+            }
+
+            rs.close();
+            stmt.close();
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("fail Database transaction");
+        }
+    }
 }
