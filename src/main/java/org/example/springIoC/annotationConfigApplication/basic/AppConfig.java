8d28db505e22b71eb0eb4c1ceb1a9df58063da75
hyeok <cksgurwkd12@naver.com>
2024-02-20 21:29:24 +0900

    feat: ** super important, can't use autowired in test case

--- src/main/java/org/example/springIoC/annotationConfigApplication/basic/AppConfig.java
+++ src/main/java/org/example/springIoC/annotationConfigApplication/basic/AppConfig.java
@@ -12,4 +12,11 @@ public class AppConfig {
         return new AcTestOrderService();
     }
 
+    /**
+     * 스프링 부트의 경우 이 과정을 컴포넌트 스캔이란 것들을 통해 대신하는데, 의존성 관리 대상인 Bean들을 @Component라는 애노테이션을 마킹해 스프링 부트가 뜰 때 컴포넌트 스캔을 통해 등록함.
+     * 그런게 아니라면 이렇게 하나하나 손으로 등록 해줘야함, 컴포넌트 스캔 없으면 사실 그냥 겁나 불편한 건데
+     *
+     * 이렇게 등록한 bean은 AnnotationConfigApplicationContext를 써서 getBean 하지 않아도 autowired로 가져올 수 있나?
+     */
+
 }
--- src/main/java/org/example/springIoC/annotationConfigApplication/basic/controller/AcTestOrderController.java
+++ src/main/java/org/example/springIoC/annotationConfigApplication/basic/controller/AcTestOrderController.java
@@ -6,10 +6,14 @@ import org.example.springIoC.annotationConfigApplication.basic.service.AcTestOrd
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AcTestOrderController {
+
+    @Autowired
+    AcTestOrderService acTestOrderServiceWithAutowired;
 
     @DisplayName("빈 애노테이션을 통해 등록된 서비스 주입받기(1): 기본 적용")
     @Test
@@ -35,6 +39,16 @@ public class AcTestOrderController {
 
         // then
         Assertions.assertEquals("상품hyeok01", acTestOrderService.findAllOrder().getName());
+    }
+
+    @DisplayName("빈 애노테이션을 통해 등록된 서비스 주입받기(3): 여기서도 autowired 적용 가능")
+    @Test
+    public void findAllType3() {
+        // 단, Test 애노테이션 적용시 스프링의 관리를 벗어나니까 @Autowired 적용 불가! 테스트 시에 빈 주입 사용하려면 Configuration을 추가해줘야함.
+        // when & then
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            acTestOrderServiceWithAutowired.findAllOrder().getName();
+        });
 
     }
 
