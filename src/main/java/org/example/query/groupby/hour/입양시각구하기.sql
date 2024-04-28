e97cf22c1ea091bfc68609418d6c02f4934816a7
hyeok <cksgurwkd12@naver.com>
2024-04-29 07:03:15 +0900

    feat: sql log about datetime, hour, group by

---
+++ src/main/java/org/example/query/groupby/hour/입양시각구하기.sql
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/59412
+
+SELECT
+    HOUR(DATETIME) AS HOUR,
+    COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR(DATETIME)
+HAVING 9 <= HOUR AND HOUR <= 19
+ORDER BY HOUR
